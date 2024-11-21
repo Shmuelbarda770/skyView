5cee0035f078aa142ea35b3aa628e853a8b32232
shmuel <shmuelbarda770@example.com>
2024-11-21 18:31:21 +0200

    Completion of first mission

---
+++ drone_comm_system.py
@@ -0,0 +1,114 @@
+
+import socket
+import threading
+import json
+import queue
+import logging
+from json_sender import send
+import pickle
+import threading
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+
+queue_size = config.getint('settings', 'queue_size')
+size_bytes_from_drone = config.getint('settings', 'size_bytes_from_drone')
+
+data_queue = queue.Queue(maxsize=queue_size)
+
+
+logging.basicConfig(level=logging.INFO, filename='test.log', filemode='a' , format='%(asctime)s - %(levelname)s - %(message)s') ## TODO: thread safe
+logger = logging.getLogger()
+
+
+
+def send_data_to_cloud(thread_01_status):
+
+    while thread_01_status.is_set():
+        try:
+            data_from_drone = data_queue.get(block=True, timeout=60)
+            if data_from_drone:
+                logger.info("Sending data to cloud")
+                json.dumps(data_from_drone)
+                # send(data_from_drone)
+            else:
+                logger.info("Waiting for data in queue")
+        except Exception as e:
+            logger.error(f"Error sending data: {e}")
+
+    logger.info("send_data_to_cloud thread stopped")
+
+
+def collect_data(thread_01_status, soc, route_id , flight_id , platform_id , platform_name , date):
+
+    # ip = config.get('settings', 'ip')
+    # port = config.getint('settings', 'port')
+    ip = "127.0.0.1"
+    port = 3000
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        soc.bind((ip, port))
+        logger.info("Successfully bound to ip and port")
+    except Exception as e:
+        logger.error("Failed to bind socket to ip and port")
+        return
+
+    soc.listen(1) ## TODO: why 1?
+    server_socket, server_address = soc.accept() ## TODO: logger to indicate that is waiting for connection, put maximum time for connection if no connection print log and keep in a while loop. 
+    logger.info("Drone connected")
+
+
+    while thread_01_status.is_set():
+        try:
+            data = server_socket.recv(size_bytes_from_drone)
+            if data:
+                data = json.loads(data)
+
+                
+
+                data["route_id"]=route_id
+                data["flight_id"]=flight_id
+                data["platform_id"]=platform_id
+                data["platform_name"]=platform_name
+                data["Date"]=date
+                print(data)
+                logger.info("Data received and information added from the inputs")
+
+            if data_queue.full():
+                data_queue.queue.clear()
+                logger.warning("Queue has been cleared")
+
+            data_queue.put(data)
+        except Exception as e:
+            logger.warning(f"{e}")
+        
+
+    server_socket.close()## TODO: make sure that the socket is close when the thead is killed
+
+
+def open_socket(thread_01_status,route_id , flight_id , platform_id , platform_name , date):
+    soc = None
+    collector_thread = threading.Thread(target=collect_data, args=(thread_01_status, soc,route_id , flight_id , platform_id , platform_name , date), daemon=True)
+    collector_thread.start()
+
+    cloud_thread = threading.Thread(target=send_data_to_cloud, args=(thread_01_status,), daemon=True)
+    cloud_thread.start()
+
+    collector_thread.join()
+    cloud_thread.join()
+
+
+def stop(thread_01_status):
+    logger.info("Stopping Connection")
+    thread_01_status.clear()
+
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,80 +1,59 @@
-import socket
 import threading
-import json
-import requests
+import flet as ft
+from flet import Page, TextField, ElevatedButton, Text, Row, Column
+from flet_core import ControlEvent
 import queue
 import logging
+from drone_comm_system import stop ,open_socket
 
 
-logging.basicConfig(level=logging.INFO, filename='test.log', filemode='a' , format='%(asctime)s - %(levelName)s - %(message)s')
+logging.basicConfig(level=logging.INFO, filename='test.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
+def main(page: Page):
+    page.title = "skyView"
+    page.window.width = 500
+    page.window.height = 500
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
 
-data_queue = queue.Queue(maxsize=100)
-
-
-CLOUD_URL = ""
-
-
-def send_data_to_cloud():
-    while True:
-        try:
-           
-            data = data_queue.get(block = True)
-           
-            # data_dict = json.dumps({"key": "value", "data": data})
-            data_dict={"data": data , "input":"input"}
-            response = requests.post(CLOUD_URL, json=data_dict)
-            if response.status_code == 200:
-                logger.info("Data sent successfully.")
-            else:
-                logger.warning(f"Failed to upload data to the cloud. Status code: {response.status_code}")
-        except Exception as e:
-            logger.error(f"Error sending data: {e}")
-
-
-def collect_data():
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = '127.0.0.1'
-    port = 3000
-    soc.bind((ip, port))
-    soc.listen(1)
-
-    logger.info("Server is listening... Waiting for connection.")
-    server_socket, server_address = soc.accept()
-    logger.info(f"Client connected: {server_address}")
-
-    while True:
-        data = server_socket.recv(1024)
-        if not data:
-            break
-        logger.info(f"Received data: {data.decode()}")
-      
-        data_queue.put(data.decode())
-
-    server_socket.close()
-
-
-def open_socket():
    
-    collector_thread = threading.Thread(target=collect_data, daemon=True)
-    collector_thread.start()
+    thread_01_status = threading.Event()
 
-    cloud_thread = threading.Thread(target=send_data_to_cloud, daemon=True)
-    cloud_thread.start()
-
-    collector_thread.join()
-    cloud_thread.join()
-
-
-def stop_socket(soc):
-    soc.close()
-
-
-def main():
-    open_socket()
-
+    route_id: TextField = TextField(label="route id", width=200, height=50, fill_color='blue-light')
     
+    flight_id: TextField = TextField(label="flight id", width=200, height=50, fill_color='blue-light')
+
+    platform_id: TextField = TextField(label="platform id", width=200, height=50, fill_color='blue-light')
+
+    platform_name: TextField = TextField(label="platform name", width=200, height=50, fill_color='blue-light')
+
+    date: TextField = TextField(label="date", width=200 , height=50, fill_color='blue-light')
+
+
+
+
+    button: ElevatedButton = ElevatedButton(text="start", width=200)
+
+    def click_handler(e: ControlEvent) -> None:
+        if thread_01_status.is_set():
+            stop(thread_01_status)
+            # page.clean()
+            button.text = 'Connecting to the drone'
+        else:
+            thread_01_status.set()
+            open_socket(thread_01_status,route_id.value , flight_id.value , platform_id.value , platform_name.value , date.value)
+            button.text = 'Stop connecting to the drone'
+        page.update()
+
+    button.on_click = click_handler
+
+    page.add(
+        Row(
+            controls=[Column([route_id , flight_id , platform_id , platform_name , date , button])],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
 
 if __name__ == "__main__":
-    main()
+    ft.app(target=main)
